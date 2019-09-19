0c1189869c3159e1605cc128b3fc4a4615d06a41
Edi <edi.gamarnik@gmail.com>
2019-09-19 19:30:10 +0300

    app2

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Title from './component/Title';
@@ -6,22 +5,29 @@ import Menu from './component/Menu';
 import Home from './component/Home';
 import About from './component/About';
 import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
 
-      <Title/>
-      <Router>
-        <Menu/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </Router>
-    </div>
+  constructor(){
 
-  );
+    super();
+    this.state={users:[]}
+  }
+
+  componentWillMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => this.setState({users:json}))
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Title/>
+      </div>
+    )
+  }
 }
-
-export default App;
